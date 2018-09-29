3aa09b2b21d255151c178994c4c05bd1f6b66051
zhaojintian <729234283@qq.com>
2018-09-29 15:08:15 +0800

    update

--- src/components/TabBar/index.vue
+++ src/components/TabBar/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="c-tab-bar">
-    <div class="c-tab-bar__item c5 fs5">
+    <div
+      class="c-tab-bar__item c5 fs5"
+      @click.stop="toggleDropVisible">
       <div class="c-icon c-icon--extend"></div>
       赵老人
     </div>
@@ -12,15 +14,50 @@
       </div>
     </div>
     <div class="c-tab-bar__item c5 fs5">我的</div>
-    <div class="cc-drop fs5">
-      <div class="cc-drop__item is-first is-active">赵老人</div>
-      <div class="cc-drop__item">陈老人</div>
+    <div
+      v-show="cDrop.visible"
+      class="cc-drop fs5">
+      <div
+        class="cc-drop__item is-first"
+        :class="{ 'is-active': cDrop.current === 0 }"
+        @click="handleSelectDrop(0)">
+        赵老人
+      </div>
+      <div
+        class="cc-drop__item"
+        :class="{ 'is-active': cDrop.current === 1}"
+        @click="handleSelectDrop(1)">
+        陈老人
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data () {
+      return {
+        cDrop: {
+          visible: false,
+          current: 0
+        }
+      }
+    },
+    methods: {
+      toggleDropVisible () {
+        this.cDrop.visible = !this.cDrop.visible
+      },
+      handleSelectDrop (index) {
+        this.cDrop.current = index
+        this.cDrop.visible = false
+      }
+    },
+    mounted () {
+      document.getElementById('app').addEventListener('click', () => {
+        this.cDrop.visible = false
+      })
+    }
+  }
 </script>
 
 <style lang="scss" src="./styles/index.scss"></style>
