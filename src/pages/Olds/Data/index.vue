d007125e05f31d8626ea84a6f5f1585fa448e240
lx160538 <734127061@qq.com>
2018-09-30 11:31:11 +0800

    更新

--- src/pages/Olds/Data/index.vue
+++ src/pages/Olds/Data/index.vue
@@ -1,6 +1,100 @@
 <template>
-  <div class="p-olds-data">
-    健康数据
+  <div class="p-olds-data bg-c2">
+    <div class="pb-wrap-data">
+      <div class="pb-wrap-data__top bg-c1">
+        <p class="fs4 pb-wrap-data__top--p">2018年1月1日</p>
+        <div class="pb-grid">
+          <ul>
+            <li>
+              <div>
+                <h2 class="fs11">100/77</h2>
+                <div>
+                  <span class="fs4 c7">血压</span>
+                  <span class="fs1 c3">mmHg</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2 class="fs11">100/77</h2>
+                <div>
+                  <span class="fs4 c7">血压</span>
+                  <span class="fs1 c3">mmHg</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2 class="fs11">100/77</h2>
+                <div>
+                  <span class="fs4 c7">血压</span>
+                  <span class="fs1 c3">mmHg</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2 class="fs11">100/77</h2>
+                <div>
+                  <span class="fs4 c7">血压</span>
+                  <span class="fs1 c3">mmHg</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2 class="fs11">100/77</h2>
+                <div>
+                  <span class="fs4 c7">血压</span>
+                  <span class="fs1 c3">mmHg</span>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div>
+                <h2 class="fs11">100/77</h2>
+                <div>
+                  <span class="fs4 c7">血压</span>
+                  <span class="fs1 c3">mmHg</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="pb-wrap-data__bottom bg-c1">
+        <table class="o-gridtable">
+          <thead>
+            <tr>
+              <th class="fs4 u-tac">名称</th>
+              <th class="fs4 u-tac">分类</th>
+              <th class="fs4 u-tac">数量</th>
+              <th class="fs4 u-tac">日期</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="fs4 c8 u-tac">医护毛巾</td>
+              <td class="fs4 c8 u-tac">医护毛巾</td>
+              <td class="fs4 c8 u-tac">2</td>
+              <td class="fs4 c8 u-tac">2018.05.06</td>
+            </tr>
+            <tr>
+              <td class="fs4 c8 u-tac">医护毛巾</td>
+              <td class="fs4 c8 u-tac">医护毛巾</td>
+              <td class="fs4 c8 u-tac">2</td>
+              <td class="fs4 c8 u-tac">2018.05.06</td>
+            </tr>
+            <tr>
+              <td class="fs4 c8 u-tac">医护毛巾</td>
+              <td class="fs4 c8 u-tac">医护毛巾</td>
+              <td class="fs4 c8 u-tac">2</td>
+              <td class="fs4 c8 u-tac">2018.05.06</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
